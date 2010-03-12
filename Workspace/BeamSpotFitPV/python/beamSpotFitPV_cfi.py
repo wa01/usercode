6dcd8d3e1fa4f2fd16af966e7821e1e002560807
adamwo <>
2010-03-12 08:25:49 +0000

    Creating a cfi file and moving cfg to /test

---
+++ Workspace/BeamSpotFitPV/python/beamSpotFitPV_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+beamSpotFitPV = cms.EDAnalyzer('BeamSpotFitPV',
+                               minNrVerticesForFit = cms.uint32(100),
+                               vertexCacheSize = cms.uint32(5000),
+                               beamspotTag = cms.InputTag("offlineBeamSpot"),
+                               vertexTag = cms.InputTag("offlinePrimaryVertices"),
+                               minVertexNdf = cms.double(4.),
+                               maxVertexNormChi2 = cms.double(10.),
+                               minVertexNTracks = cms.uint32(0),
+                               minVertexMeanWeight = cms.double(0.5),
+                               maxVertexR = cms.double(2),
+                               maxVertexZ = cms.double(30),
+                               errorScale = cms.double(0.9),
+                               nSigmaCut = cms.double(5.)
+                               )
+
--- Workspace/BeamSpotFitPV/test/beamSpotFitPV.py
+++ Workspace/BeamSpotFitPV/test/beamSpotFitPV.py
@@ -19,20 +19,7 @@ process.TFileService = cms.Service("TFileService",
                                    closeFileFast = cms.untracked.bool(True)
                                    )
 
-process.demo = cms.EDAnalyzer('BeamSpotFitPV',
-  minNrVerticesForFit = cms.uint32(100),
-  vertexCacheSize = cms.uint32(5000),
-  beamspotTag = cms.InputTag("offlineBeamSpot"),
-  vertexTag = cms.InputTag("offlinePrimaryVertices"),
-  minVertexNdf = cms.double(4.),
-  maxVertexNormChi2 = cms.double(10.),
-  minVertexNTracks = cms.uint32(0),
-  minVertexMeanWeight = cms.double(0.5),
-  maxVertexR = cms.double(999),
-  maxVertexZ = cms.double(999),
-  errorScale = cms.double(0.9),
-  nSigmaCut = cms.double(3.)
-)
+process.load("Workspace.BeamSpotFitPV.beamSpotFitPV_cfi")
 
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.beamSpotFitPV)
