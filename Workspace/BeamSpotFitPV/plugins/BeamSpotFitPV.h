3f6b5d5afd4d22e6b2f9d8080650f7f9125e1bc6
adamwo <>
2010-03-17 18:13:22 +0000

    take TFileService from right place

--- Workspace/BeamSpotFitPV/plugins/BeamSpotFitPV.h
+++ Workspace/BeamSpotFitPV/plugins/BeamSpotFitPV.h
@@ -22,7 +22,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
