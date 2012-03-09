d2931759e662fe2ca5bdd73d224a9e838a33f64d
adamwo <>
2012-03-09 15:57:40 +0000

    *** empty log message ***

--- Workspace/Limits/test/convertMsugraBeff.py
+++ Workspace/Limits/test/convertMsugraBeff.py
@@ -11,33 +11,39 @@ from string import Template
 # get effect of scale factor (defined as SF1 / SF0)
 #   if necessary : sum over b-tag bins
 #
-def getFactor (dic,btags):
+def getFactor (dic,signal,btags):
     norm_sf0 = 0.
     norm_sf1 = 0.
-    for btag in btags:
-        norm_sf0 += dic['Norm_sf0'][btag][0]
-        norm_sf1 += dic['Norm_sf1'][btag][0]
+    for lep in dic:
+        for btag in btags:
+            norm_sf0 += dic[lep][signal]['Norm_sf0'][btag][0]
+            norm_sf1 += dic[lep][signal]['Norm_sf1'][btag][0]
     if norm_sf0 < 0.000001:  return None
     return norm_sf1/norm_sf0
 #
 # get variation on SF1 (up-down)/(2*nominal)
 #   if necessary : sum over b-tag bins
 #
-def getVar (dic,btags,mode):
+def getVar (dic,signal,btags,mode):
     norm_sf1 = 0.
     up = 0.
     down = 0.
     strup = 'Up_'+mode+'_sf1'
     strdown = 'Down_'+mode+'_sf1'
-    for btag in btags:
-        norm_sf1 += dic['Norm_sf1'][btag][0]
-        up += dic[strup][btag][0]
-        down += dic[strdown][btag][0]
+    s1 = 0.
+    for lep in dic:
+        for btag in btags:
+            norm_sf1 += dic[lep][signal]['Norm_sf1'][btag][0]
+            up += dic[lep][signal][strup][btag][0]
+            down += dic[lep][signal][strdown][btag][0]
+            if mode == 'b':
+                s1 += dic[lep][signal]['Norm_sf1'][btag][0]*dic[lep][signal]['BDelta/(2Norm_sf1)'][btags[0]][0]
+            else:
+                s1 += dic[lep][signal]['Norm_sf1'][btag][0]*dic[lep][signal]['LDelta/(2Norm_sf1)'][btags[0]][0]
     var = (up-down)/2./norm_sf1
     if len(btags) == 1:
-        if ( mode == 'b' and abs(var-dic['BDelta/(2Norm_sf1)'][btags[0]][0]) > 0.0001 ) or \
-           ( mode == 'l' and abs(var-dic['LDelta/(2Norm_sf1)'][btags[0]][0]) > 0.0001 ):
-            print "Mismatch in variations: ",btags[0],mode,var,dic['BDelta/(2Norm_sf1)'][btags[0]][0],dic['LDelta/(2Norm_sf1)'][btags[0]][0]
+        if abs(var-s1/norm_sf1) > 0.0001:
+            print "Mismatch in variations: ",btags[0],mode,var,s1/norm_sf1
             return None
     return var
 
@@ -61,44 +67,43 @@ btagLabels = { 'b0' : [ '0' ], 'b1' : [ '1' ], 'b1p' : [ '1' , '>=2' ],  'b2' : 
 # create dictionary and loop over flavours, HT and MET cuts
 #
 effDict = {}
-for lepton in leptons:
-    if lepton == 'Muon':
-        lep = 'Mu'
-    else:
-        lep = 'Ele'
-    if not lep in effDict:  effDict[lep] = {}
-    for ht in hts:
-        if not ht in effDict[lep]:  effDict[lep][ht] = {}
-        for met in mets:
-            if not met in effDict[lep][ht]:  effDict[lep][ht][met] = {}
+for ht in hts:
+    if not ht in effDict:  effDict[ht] = {}
+    for met in mets:
+        if not met in effDict[ht]:  effDict[ht][met] = {}
+        inDict = { 'Muon' : {}, 'Electron' : {} }
+        for lepton in inDict:
             ifname = template.substitute(lepton=lepton,ht=str(ht),met=str(met))
             if not os.path.exists(ifname):
                 print "No such file ",ifname
             print ifname
             # execute input file
             execfile(ifname)
-            # unused label
-            for label in nbtags:
-                # msugra points
-                for signal in nbtags[label]:
-                    # translate to standard string
-                    msugraString = signal.replace("signal_","msugra_")
-                    msugraString += "_10_0_1"
-                    if not msugraString in effDict[lep][ht][met]: effDict[lep][ht][met][msugraString] = {}
+            labels = nbtags.keys()
+            assert(len(labels)==1)
+            inDict[lepton] = nbtags[labels[0]]
+        # unused label
+        for label in nbtags:
+            # msugra points
+            for signal in nbtags[label]:
+                # translate to standard string
+                msugraString = signal.replace("signal_","msugra_")
+                msugraString += "_10_0_1"
+                if not msugraString in effDict[ht][met]: effDict[ht][met][msugraString] = {}
 #                    btags = nbtags[label][signal]['originalMC'].keys()
 #                    print lep,ht,met,msugraString
-                    # btag bins (output / input notation)
-                    for btagOut, btagsIn in btagLabels.iteritems():
-                        # scaling after application of SF
-                        sfFactor = getFactor(nbtags[label][signal],btagsIn)
-                        # skip empty bins
-                        if sfFactor == None:  continue
-                        # add correction factor and variations to dictionary
-                        if not btagOut in effDict[lep][ht][met][msugraString]:
-                            effDict[lep][ht][met][msugraString][btagOut] = {}
-                        effDict[lep][ht][met][msugraString][btagOut]['sfFactor'] = sfFactor
-                        effDict[lep][ht][met][msugraString][btagOut]['relVarB'] = getVar(nbtags[label][signal],btagsIn,'b')
-                        effDict[lep][ht][met][msugraString][btagOut]['relVarL'] = getVar(nbtags[label][signal],btagsIn,'l')
+                # btag bins (output / input notation)
+                for btagOut, btagsIn in btagLabels.iteritems():
+                    # scaling after application of SF
+                    sfFactor = getFactor(inDict,signal,btagsIn)
+                    # skip empty bins
+                    if sfFactor == None:  continue
+                    # add correction factor and variations to dictionary
+                    if not btagOut in effDict[ht][met][msugraString]:
+                        effDict[ht][met][msugraString][btagOut] = {}
+                    effDict[ht][met][msugraString][btagOut]['sfFactor'] = sfFactor
+                    effDict[ht][met][msugraString][btagOut]['relVarB'] = getVar(inDict,signal,btagsIn,'b')
+                    effDict[ht][met][msugraString][btagOut]['relVarL'] = getVar(inDict,signal,btagsIn,'l')
 #                print effDict
 #                sys.exit(0)
 #
