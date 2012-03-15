c538df7baaa907a500282bc35d059230c3f8f5f3
adamwo <>
2012-03-15 13:31:17 +0000

    change to write corrected eventCounts

--- Workspace/Limits/test/effSmoothedRatios.py
+++ Workspace/Limits/test/effSmoothedRatios.py
@@ -1,11 +1,12 @@
-import pickle
+import cPickle
 import ROOT
 import math
 import sys
 #
 # read efficiencies from dictionary, smooth using
 #   the EfficiencyUtils.C macro and write ratios
-#   to new dictionary
+#   to new dictionary. If 2 arguments: rescale
+#   event counts in 2nd file with ratios
 #
 
 #
@@ -33,8 +34,12 @@ def findRange (ms):
 # create dictionary with ratio of smoothed / raw efficiencies
 #   argument: dictionary with raw efficiencies
 #
-filename = sys.argv[1]
-mydic = pickle.load(file(filename))
+fnameEff = sys.argv[1]
+effdic = cPickle.load(file(fnameEff))
+
+fnameEvt = None
+if len(sys.argv) > 2:
+  fnameEvt = sys.argv[2]
 
 from ROOT import gROOT
 gROOT.ProcessLine(".L EfficiencyUtils.C+")
@@ -44,15 +49,15 @@ allRatios = {}
 
 # loop over all btag bins, ht and met values
 # in input dictionary
-for btag in mydic:
-  for ht in mydic[btag]:
-    for met in mydic[btag][ht]:
+for btag in effdic:
+  for ht in effdic[btag]:
+    for met in effdic[btag][ht]:
       # create lists of M0 and M12 values and
       #   set of process names, if applicable
       m0s = [ ]
       m12s = [ ]          
       processes = set([ ])
-      for msugra in mydic[btag][ht][met]:
+      for msugra in effdic[btag][ht][met]:
         # extract M0 and M12 from msugra string
         parts = msugra.split('_')
         m0 = int(parts[1])
@@ -60,8 +65,8 @@ for btag in mydic:
         m12 = int(parts[2])
         if not m12 in m12s:  m12s.append(m12)
         # if dictionary contains processes: add to list
-        if type(mydic[btag][ht][met][msugra]) is dict:
-          processes.update(mydic[btag][ht][met][msugra].keys())
+        if type(effdic[btag][ht][met][msugra]) is dict:
+          processes.update(effdic[btag][ht][met][msugra].keys())
           
 #
 # extraction of ranges and bin widths
@@ -85,11 +90,11 @@ print processes
 
 # loop over all btag bins, ht and met values in input dictionary
 #   and create corresponding levels in output dictionary
-for btag in mydic:
+for btag in effdic:
   if not btag in allRatios:  allRatios[btag] = {}
-  for ht in mydic[btag]:
+  for ht in effdic[btag]:
     if not ht in allRatios[btag]:  allRatios[btag][ht] = {}
-    for met in mydic[btag][ht]:
+    for met in effdic[btag][ht]:
       if not met in allRatios[btag][ht]:  allRatios[btag][ht][met] = {}
       #
       # create histograms (grid points at bin centre)
@@ -116,26 +121,26 @@ for btag in mydic:
         hRaw = ROOT.TH2F(hname,hname,nb0,fm0Min,fm0Max,nb12,fm12Min,fm12Max)
         #  print "h","h",nb0,fm0Min,fm0Max,nb12,fm12Min,fm12Max
         #  fill histogram with valid points (add entries / process to output dictionary, if necessary)
-        for msugra in mydic[btag][ht][met]:
+        for msugra in effdic[btag][ht][met]:
           parts = msugra.split('_')
           v = None
           if p == None:
-            v = mydic[btag][ht][met][msugra]
-          elif p in mydic[btag][ht][met][msugra]:
-            v = mydic[btag][ht][met][msugra][p]
+            v = effdic[btag][ht][met][msugra]
+          elif p in effdic[btag][ht][met][msugra]:
+            v = effdic[btag][ht][met][msugra][p]
             allRatios[btag][ht][met][msugra][p] = {}
           if not v == None and not math.isnan(v):
             hRaw.Fill(float(parts[1]),float(parts[2]),v)
         #
         # create histogram with ratios and fill bin contents into dictionary
         #
-        hRatio = ROOT.doAll(hRaw)
+        hRatio = ROOT.doEff(hRaw)
         nbx = hRatio.GetNbinsX()
         nby = hRatio.GetNbinsY()
         for ix in range(1,nbx):
           for iy in range(1,nby):
             ratio = hRatio.GetBinContent(ix,iy)
-            if ratio < 0.01:  continue
+            if abs(ratio) < 0.000001:  continue
             m0 = int(hRatio.GetXaxis().GetBinCenter(ix)+0.5)
             m12 = int(hRatio.GetYaxis().GetBinCenter(iy)+0.5)
             msugra = 'msugra_'+str(m0)+'_'+str(m12)+'_10_0_1'
@@ -147,20 +152,57 @@ for btag in mydic:
         gROOT.Clear()
 #        gROOT.ls()
 #
+# rescale event yields
+#
+if fnameEvt != None:
+  evdic = cPickle.load(file(fnameEvt))
+  allEvts = {}
+  for btag in allRatios:
+    if not btag in evdic:  continue
+    allEvts[btag] = {}
+    for ht in allRatios[btag]:
+      if not ht in evdic[btag]:  continue
+      allEvts[btag][ht] = {}
+      for met in allRatios[btag][ht]:
+        if not met in evdic[btag][ht]:  continue
+        allEvts[btag][ht][met] = {}
+        for msugra in allRatios[btag][ht][met]:
+          if not msugra in evdic[btag][ht][met]:  continue
+          allEvts[btag][ht][met][msugra] = {}
+          for p in processes:
+            if p == None:
+              allEvts[btag][ht][met][msugra] = evdic[btag][ht][met][msugra]*allRatios[btag][ht][met][msugra]
+            else:
+              if not p in evdic[btag][ht][met][msugra]:  continue
+              allEvts[btag][ht][met][msugra][p] = evdic[btag][ht][met][msugra][p]* \
+                                                  allRatios[btag][ht][met][msugra][p]
+            
+#
 # write output dictionary (derive name from input file name)
 #
 import os.path
-fname = filename
-ind = fname.rfind("/")
-if ind != -1:  fname = fname[ind+1:]
-ind = fname.find(".")
-if ind != -1:  fname = fname[0:ind]
-fname += "-EffRatio.pkl"
+if fnameEvt == None:
+  fname = fnameEff
+  ind = fname.rfind("/")
+  if ind != -1:  fname = fname[ind+1:]
+  ind = fname.find(".")
+  if ind != -1:  fname = fname[0:ind]
+  fname += "-Ratio.pkl"
+else:
+  fname = fnameEvt
+  ind = fname.rfind("/")
+  if ind != -1:  fname = fname[ind+1:]
+  ind = fname.find(".")
+  if ind != -1:  fname = fname[0:ind]
+  fname += "-smoothed.pkl"
 if os.path.exists(fname):
   print fname," exists"
   sys.exit(1)
   
 f = open(fname,"w")
-pickle.dump(allRatios,f)
+if fnameEvt == None:
+  cPickle.dump(allRatios,f)
+else:
+  cPickle.dump(allEvts,f)
 f.close()
 
