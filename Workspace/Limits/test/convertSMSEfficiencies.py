4263ed32435b1e04852ee51d7ff2599af74a8ce1
adamwo <>
2012-04-04 02:04:41 +0000

    added option for event yields

--- Workspace/Limits/test/convertSMSEfficiencies.py
+++ Workspace/Limits/test/convertSMSEfficiencies.py
@@ -5,9 +5,14 @@ import os
 # convert the SMS efficiencies from files in /data/schoef/efficiencies/*_Efficiency.py
 #   to "msugra-like" dictionaries
 #
-
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("--xsName", dest="xsname", default=None, type="string", action="store")
+parser.add_option("--xsHisto", dest="xshisto", default="gluino", type="string", action="store")
+parser.add_option("--lumi", dest="lumi", default=4700., type="float", action="store", help="luminosity (pb-1)")
+(options, args) = parser.parse_args()
 # input file
-fnameEff = sys.argv[1]
+fnameEff = args[0]
 execfile(fnameEff)
 #
 # loop over input dictionary
@@ -55,3 +60,41 @@ for mass in effDicts:
     cPickle.dump(effDicts[mass],fout)
     fout.close()
 
+#
+#
+#
+if options.xsname != None:
+    import ROOT
+    xsfile = ROOT.TFile(options.xsname)
+    xsTH1 = xsfile.Get(options.xshisto)
+    xsecs = {}
+    evtDicts = {}
+    nb = xsTH1.GetNbinsX()
+    for i in range(1,nb+1):
+        x = int(xsTH1.GetXaxis().GetBinLowEdge(i)+0.5)
+        xsecs[x] = options.lumi*xsTH1.GetBinContent(i)
+
+    import copy
+    for mass in effDicts:
+        evtDict = copy.deepcopy(effDicts[mass])
+        for btag in evtDict:
+            for ht in evtDict[btag]:
+                for met in evtDict[btag][ht]:
+                    for key in evtDict[btag][ht][met]:
+                        parts = key.split("_")
+                        m0 = int(parts[1])
+                        evtDict[btag][ht][met][key] *= xsecs[m0]
+        
+        suffix = ""
+        # if more than one intermediate key or a float: create suffix for model name
+        if len(effDicts.keys()) > 1 or type(mass) == float or type(mass) == int:
+            suffix = str(mass).replace(".","")
+        fname = fname0 + suffix + "_NLO_events.pkl"
+        # don't overwrite output file
+        if os.path.exists(fname):
+            print "output file ",fname," exists"
+            continue
+        # dump the dictionary for the intermediate key
+        fout = open(fname,"wb")
+        cPickle.dump(evtDict,fout)
+        fout.close()
